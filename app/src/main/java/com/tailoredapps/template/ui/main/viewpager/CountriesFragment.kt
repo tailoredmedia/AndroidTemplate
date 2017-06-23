fc9f6cc3301b2547ede475c25a523a31c7ccb5bd
patloew <patloew@users.noreply.github.com>
2017-06-23 10:59:44 +0200

    Add Kotlin example.

---
+++ app/src/main/java/com/tailoredapps/template/ui/main/viewpager/CountriesFragment.kt
@@ -0,0 +1,54 @@
+package com.tailoredapps.template.ui.main.viewpager
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.tailoredapps.template.R
+import com.tailoredapps.template.databinding.FragmentRecyclerviewBinding
+import com.tailoredapps.template.ui.base.BaseFragment
+import com.tailoredapps.template.ui.base.viewmodel.MvvmViewModel
+import com.tailoredapps.template.ui.main.recyclerview.CountryAdapter
+
+import javax.inject.Inject
+
+/* Copyright 2016 Patrick Löwenstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * FILE MODIFIED 2017 Tailored Media GmbH*/
+abstract class CountriesFragment<V : MvvmViewModel<CountriesView>> : BaseFragment<FragmentRecyclerviewBinding, V>(), CountriesView {
+
+    @Inject protected lateinit var adapter: CountryAdapter
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return setAndBindContentView(inflater, container, savedInstanceState, R.layout.fragment_recyclerview)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+    }
+
+    // View
+
+    override fun onRefresh(success: Boolean) {
+        binding.swipeRefreshLayout.isRefreshing = false
+    }
+
+}
