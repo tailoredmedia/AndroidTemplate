9b166d01ecc41afabe25aa7253de6c391e15256a
floschu <floschu@users.noreply.github.com>
2018-03-12 10:50:01 +0100

    added Intent extensions

---
+++ app/src/main/java/com/tailoredapps/template/util/extensions/IntentExtensions.kt
@@ -0,0 +1,24 @@
+/* Copyright 2017 Tailored Media GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+package com.tailoredapps.template.util.extensions
+
+import android.content.Intent
+import android.net.Uri
+
+
+fun Intent.web(url: String): Intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+fun Intent.mail(mail: String): Intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$mail"))
+fun Intent.call(number: String): Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$number"))
+fun Intent.maps(location: String): Intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.co.in/maps?q=$location"))
