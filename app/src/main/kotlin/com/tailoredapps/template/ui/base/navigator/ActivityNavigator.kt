17c99431380971ef86d14548cd88389db3115332
floschu <floschu@users.noreply.github.com>
2018-07-02 11:27:22 +0200

    Hotfix: Navigator now calls setResult before finishing after Transition.

--- app/src/main/kotlin/com/tailoredapps/template/ui/base/navigator/ActivityNavigator.kt
+++ app/src/main/kotlin/com/tailoredapps/template/ui/base/navigator/ActivityNavigator.kt
@@ -39,6 +39,7 @@ open class ActivityNavigator(protected val activity: FragmentActivity) : Navigat
     }
 
     override fun finishActivityAfterTransition() {
+        activity.setResult(Activity.RESULT_OK)
         activity.supportFinishAfterTransition()
     }
 
